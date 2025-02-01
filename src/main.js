69428bd3e280f20c6903708f59c7600368f8e6cd
Vishal Tiwadi <118591861+Vishal09Software@users.noreply.github.com>
2025-02-01 09:01:25 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './assets/style.css'
 import { createApp } from 'vue'
+import './assets/style.css'
 import App from './App.vue'
 import router from './router.js';
 import 'primeicons/primeicons.css'
